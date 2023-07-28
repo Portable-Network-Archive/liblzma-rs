bdf7ae15137b6010ad7bf9287ea610fa3edde957
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-07-29 01:18:34 +0900

    Add `threading` feature

--- src/stream.rs
+++ src/stream.rs
@@ -32,6 +32,7 @@ pub struct LzmaOptions {
 }
 
 /// Builder to create a multi-threaded stream encoder.
+#[cfg(feature = "threading")]
 pub struct MtStreamBuilder {
     raw: lzma_sys::lzma_mt,
     filters: Option<Filters>,
@@ -689,6 +690,7 @@ impl Filters {
     }
 }
 
+#[cfg(feature = "threading")]
 impl MtStreamBuilder {
     /// Creates a new blank builder to create a multithreaded encoding `Stream`.
     pub fn new() -> MtStreamBuilder {
