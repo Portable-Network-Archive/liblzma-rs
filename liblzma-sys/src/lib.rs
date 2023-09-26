a8e42cd6fd5d23ff3d97889fbecdbf3c7b497c1b
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-09-27 00:14:50 +0900

    :sparkles: Add `bindgen` feature
    `bindgen --no-doc-comments xz/src/liblzma/api/lzma.h > src/bindgen.rs`

--- liblzma-sys/src/lib.rs
+++ liblzma-sys/src/lib.rs
@@ -1,5 +1,13 @@
 #![allow(bad_style)]
 
+#[cfg(feature = "bindgen")]
+mod bindgen;
+#[cfg(feature = "bindgen")]
+mod bindgen_wrap;
+#[cfg(not(feature = "bindgen"))]
 mod manual;
 
+#[cfg(feature = "bindgen")]
+pub use bindgen_wrap::*;
+#[cfg(not(feature = "bindgen"))]
 pub use manual::*;
