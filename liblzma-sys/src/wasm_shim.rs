230f6c7e917ea2d6a7393c977bad228a10f3e83c
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2023-10-06 22:15:59 +0900

    :bug: fix compile to `wasm32-unknown-unknown`, `wasm32-wasi`

---
+++ liblzma-sys/src/wasm_shim.rs
@@ -0,0 +1,25 @@
+use std::alloc::{alloc, dealloc, Layout};
+use std::os::raw::c_void;
+
+#[no_mangle]
+pub extern "C" fn rust_lzma_wasm_shim_malloc(size: usize) -> *mut c_void {
+    unsafe {
+        let layout = Layout::from_size_align_unchecked(size, 1);
+        alloc(layout).cast()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn rust_lzma_wasm_shim_calloc(nmemb: usize, size: usize) -> *mut c_void {
+    unsafe {
+        let layout = Layout::from_size_align_unchecked(size * nmemb, 1);
+        alloc(layout).cast()
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_lzma_wasm_shim_free(ptr: *mut c_void) {
+    // layout is not actually used
+    let layout = Layout::from_size_align_unchecked(1, 1);
+    dealloc(ptr.cast(), layout);
+}
