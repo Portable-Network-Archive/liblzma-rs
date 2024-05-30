16a4bfdfd8339609e875df508b2aae5d7a5b63dd
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2024-05-30 15:57:15 +0900

    Bump minimal supported system liblzma version to 5.6.2

--- liblzma-sys/build.rs
+++ liblzma-sys/build.rs
@@ -3,7 +3,7 @@ use std::fs;
 use std::path::PathBuf;
 
 const SKIP_FILENAMES: &[&str] = &["crc32_small", "crc64_small"];
-const MIN_LIBLZMA: &str = "5.6.0";
+const MIN_LIBLZMA: &str = "5.6.2";
 
 fn main() {
     let target = env::var("TARGET").unwrap();
