4b40bbaf8c498caf701eef3e8f474c6ba2e369cf
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2025-09-28 11:38:14 +0900

    :bookmark: Bump liblzma version to 0.4.5

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@
 //! async-compression = { version = "0.4", features = ["lzma"] }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/liblzma/0.4.4")]
+#![doc(html_root_url = "https://docs.rs/liblzma/0.4.5")]
 #![deny(missing_docs)]
 
 use std::io::{self, prelude::*};
